8d25106940e262360b3f14e45ca05f3a9dae8087
arang <dkfkddl95@naver.com>
2023-11-06 15:58:14 +0900

    수정사항7

--- src/main/java/com/seniorjob/seniorjobserver/config/CorsConfig.java
+++ src/main/java/com/seniorjob/seniorjobserver/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // localhost:5173 에서만 요청을 허용
+                .allowedOrigins("http://localhost:63342") // localhost:5173 에서만 요청을 허용63342
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
